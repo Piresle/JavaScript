ec1c4d1f5bda10c34aecc198455951260cf4e122
Leticia Pires <piresleticia@yahoo.com>
2023-04-27 18:17:34 -0300

    att ex002
    "Verificador de Idade" [Erro]

---
+++ Exercícios/Ex002/scriptEx002.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByTagName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'bebeM.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovemM.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'adultoM.png')
+            } else {
+                // idoso 
+                img.setAttribute('src', 'idosoM.png')
+            }
+        } else {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'bebeF.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovemF.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'adultoF.png')
+            } else {
+                // idoso 
+                img.setAttribute('src', 'idosoF.png')
+            }
+        }
+        res.computedStyleMap.textAlign = 'center'
+        res.innerHTML = `detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
