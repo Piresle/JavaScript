5c61c1abb7d8e7d6bb3d9c3dbdcb0279729f65fa
Leticia Pires <piresleticia@yahoo.com>
2023-05-02 17:55:15 -0300

    Ex002 erro

--- Exercícios/Ex002/scriptEx002.js
+++ Exercícios/Ex002/scriptEx002.js
@@ -3,7 +3,7 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || fano.value > ano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByTagName('radsex')
