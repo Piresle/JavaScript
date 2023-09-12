25fe41c72a57739d824eef9ec6f7198fdb1756dd
Leticia Pires <piresleticia@yahoo.com>
2023-09-12 11:24:55 -0300

    ex 006

---
+++ Exercícios/ex006.js
@@ -0,0 +1,12 @@
+// Aulas 11 e 12
+
+console.log('O console funcionou corretamente!')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}`)
+
+if (vel >= 60) {
+    console.log('Você ultrapassou a velociade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
+
