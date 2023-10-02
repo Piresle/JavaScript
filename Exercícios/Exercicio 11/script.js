13013d219f4028b0828bff47b1842fe1c6a8d65e
Leticia Pires <piresleticia@yahoo.com>
2023-10-02 18:05:53 -0300

    verif

--- Exercícios/Exercicio 11/script.js
+++ Exercícios/Exercicio 11/script.js
@@ -6,7 +6,41 @@ function verificar() {
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        window.alert('Tudo ok.')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        
+        if (fsex[0].checked) {
+            // HOMEM
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'crianca-m.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem-m.jpg')
+            } else if (idade < 58) {
+                img.setAttribute('src', 'adulto-m.jpg')
+            } else {
+                img.setAttribute('src', 'idoso-m.jpg')
+            }
+        } else if (fsex[1].checked) {
+            // MULHER
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'crianca-f.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem-f.jpg')
+            } else if (idade < 58) {
+                img.setAttribute('src', 'adulto-f.jpg')
+            } else {
+                img.setAttribute('src', 'idoso-f.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
 
