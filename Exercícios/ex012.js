206551e604deec2d83b978489cd51b8d3b84433c
Leticia Pires <piresleticia@yahoo.com>
2023-10-03 11:32:39 -0300

    ex 012

---
+++ Exercícios/ex012.js
@@ -0,0 +1,21 @@
+/* AULAS 13 E 14 */
+
+/* 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+var c = 1 //contador
+while (c <= 6) { //enquanto c for menor que 6...
+    console.log('Tudo bem?')
+    c++
+}
+
+// --------------------
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) // faça ... enquanto c for menor ou igual a 6
